f330f43b42ec9ae056a37e037ed1e716684a7eaf
Finlay Sanders <121633244+FinlaySanders@users.noreply.github.com>
2026-03-19 11:16:46 +0000

    fmt

--- crates/sdk/src/lib.rs
+++ crates/sdk/src/lib.rs
@@ -3,7 +3,11 @@ mod domain;
 mod inbound;
 mod outbound;
 
-pub type Run = inbound::run::Run<domain::service::Service<outbound::wal::WalManagerChoice>>;
+use domain::service::Service;
+use inbound::run;
+use outbound::wal::WalManagerChoice;
+
+pub type Run = run::Run<Service<WalManagerChoice>>;
 
 impl Run {
     pub fn builder() -> builder::RunBuilder {
@@ -15,7 +19,7 @@ pub use builder::RunBuilder;
 pub use domain::service::SdkService;
 pub use inbound::error::SdkError as PhotonSdkError;
 pub use inbound::run::RunStats;
+pub use outbound::wal::WalChoice;
 pub use photon_core::types::id::RunId;
 pub use photon_protocol::codec::CodecChoice;
-pub use outbound::wal::WalChoice;
 pub use photon_protocol::compressor::CompressorChoice;
